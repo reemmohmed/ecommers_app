7e047fca679739dbadd270409375b952150e797d
reemmohmed <reemmohmedmohmed@gmail.com>
2025-02-04 11:18:28 +0200

    creat SucsessSignupController and   SucsessresetpawswordContoller

---
+++ lib/controller/auth_Controller/sucsess_signup_controller.dart
@@ -0,0 +1,13 @@
+import 'package:ecommers_app/core/constant/app_Routs/name_routs.dart';
+import 'package:get/get.dart';
+
+abstract class SucsessSignupController extends GetxController {
+  goToLongin();
+}
+
+class SucsessSignupControllerImple extends SucsessSignupController {
+  @override
+  goToLongin() {
+    Get.offAllNamed(NameRouts.logen);
+  }
+}
---
+++ lib/controller/auth_Controller/sucsessresetpawsword_contoller.dart
@@ -0,0 +1,13 @@
+import 'package:ecommers_app/core/constant/app_Routs/name_routs.dart';
+import 'package:get/get.dart';
+
+abstract class SucsessresetpawswordContoller extends GetxController {
+  goToLongin();
+}
+
+class SucsessresetpawswordContollerImple extends SucsessresetpawswordContoller {
+  @override
+  goToLongin() {
+    Get.offAllNamed(NameRouts.logen);
+  }
+}
